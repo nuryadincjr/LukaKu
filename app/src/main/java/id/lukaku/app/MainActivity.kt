67a251905e0a0519a6da183d13c89819ab45f4b5
nuryadincjr <nuryadin.cjr@gmail.com>
2023-07-07 21:47:15 +0700

    thames & splashscreen

--- app/src/main/java/id/lukaku/app/MainActivity.kt
+++ app/src/main/java/id/lukaku/app/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import id.lukaku.app.ui.theme.LukaKuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             LukaKuTheme {
                 // A surface container using the 'background' color from the theme
